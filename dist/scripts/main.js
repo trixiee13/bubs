ea4b2a0410328e9a99ae2731ce7cd97995a799d9
beatrice <beatrice.g.eftimiu@gmail.com>
2020-03-07 19:03:57 +0200

    Revert "Delete dist"
    This reverts commit 70ddf70ce6a4f7963244d3f0c9219b4d34e70abc.

---
+++ dist/scripts/main.js
@@ -0,0 +1,12 @@
+console.log('yay');
+
+function dsfsdfs() {
+  console.log('aaa');
+  console.log('format');
+}
+
+const dostuff = () => {
+  console.log('scroll');
+};
+
+window.addEventListener('scroll', dostuff);
